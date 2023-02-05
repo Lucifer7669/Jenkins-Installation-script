c740e902abd7996d2d6ad7e66fbe0d710f27e311
root <root@ip-172-31-7-28.ap-south-1.compute.internal>
2023-02-05 11:34:51 +0000

    jenkins install script

---
+++ comds.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+yum install git -y
+yum install java-1.8.0-openjdk-devel.x86_64 -y
+cd /mnt
+mkdir server tool job
+cd server/
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.71/bin/apache-tomcat-9.0.71.zip
+unzip apache-tomcat-9.0.71.zip
+cd apache-tomcat-9.0.71/webapps
+wget https://get.jenkins.io/war-stable/2.346.3/jenkins.war
+cd ../bin
+chmod -R 777 *
+./startup.sh
