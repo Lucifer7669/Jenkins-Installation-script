a9afed5679178428a00c0efc1138d67413fdc24b
Lucifer7669 <112551708+Lucifer7669@users.noreply.github.com>
2023-02-07 22:44:32 +0530

    Update and rename httpd.sh to Slave-Tomcat-Instl.sh

---
+++ Slave-Tomcat-Instl.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir /mnt/server
+cd server
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.71/bin/apache-tomcat-9.0.71.zip
+unzip apache-tomcat-9.0.71.zip
+
--- httpd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-yum install git -y
-yum install java-1.8.0-openjdk-devel.x86_64 -y
-yum install httpd -y
-service httpd start 
