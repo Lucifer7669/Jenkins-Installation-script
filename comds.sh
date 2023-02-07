8675e7177531cfbe76ebd63f5d1c07b1042bd444
Lucifer7669 <112551708+Lucifer7669@users.noreply.github.com>
2023-02-07 23:42:13 +0530

    Update comds.sh

--- comds.sh
+++ comds.sh
@@ -7,3 +7,5 @@ mkdir server tool job
 cd server/
 wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.71/bin/apache-tomcat-9.0.71.zip
 unzip apache-tomcat-9.0.71.zip
+chmod 777 apache-tomcat-9.0.71/bin/*
+bash apache-tomcat-9.0.71/bin/startup.sh
